4e7b53e059f03ab8ae3244f4fbe7c65411c60777
Stephania Thomas <sthomas@marketplace.org>
2015-01-22 13:38:56 -0800

    Add API urls

--- lib/delicious.rb
+++ lib/delicious.rb
@@ -1,5 +1,9 @@
-require "delicious/version"
+require_relative "delicious/version"
+require_relative "delicious/tag"
+require_relative "delicious/bundle"
 
 module Delicious
   # Your code goes here...
+  API_URL = "http://feeds.delicious.com"
+  API_OAUTH_URL = "http://username:password@feeds.delicious.com/v1/json/tags/bundles/all?"
 end
